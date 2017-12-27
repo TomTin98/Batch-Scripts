a2e8dd8c48aebf5efbccfecd4a3bb69796aaa9fe
TomTin98 <tommy.tindeland98@gmail.com>
2017-12-27 09:22:43 +0100

    Changed code

--- Restart Print Spooler.bat
+++ Restart Print Spooler.bat
@@ -1,7 +1,9 @@
-@ECHO.
-@ECHO Stopping services, please wait for 10 secondsâ€¦
 @ECHO OFF
+
+ECHO.
+ECHO Stopping services, please wait for 10 seconds…
 NET STOP Spooler
+
 TIMEOUT /t 10 /NOBREAK
 ECHO.
 NET START Spooler
